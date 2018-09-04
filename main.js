e3739893f076a87e96882aea40daba2e6ee83df7
danielpinoga <daniel.pino@generalassemb.ly>
2018-09-04 15:08:21 -0400

    dynamically creating letter display

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 $(() => {
-  console.log("WHATS UP?")
+  console.log("JS file starting")
+  const letterArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+
+  letterArray.forEach(letter => {
+    const letterDiv = `<div class='letter' id=${letter}>${letter}</div>`
+    $('#letters').append(letterDiv)
+
+  })
 })
